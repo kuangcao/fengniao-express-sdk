6e9b5779f0f945e7df46dadf40e4b3a6e1185af1
wanglei <wanglei@liziba.com>
2016-12-29 10:54:30 +0800

    蜂鸟配送添加异常信息

--- src/main/java/com/jiabangou/fnexpresssdk/exception/FnExpressErrorException.java
+++ src/main/java/com/jiabangou/fnexpresssdk/exception/FnExpressErrorException.java
@@ -6,22 +6,40 @@ package com.jiabangou.fnexpresssdk.exception;
 public class FnExpressErrorException extends Exception {
 
     protected int code;
+    protected String requestString;
+    protected String responseString;
+
 
     public FnExpressErrorException(int code, String message) {
         super(message);
         this.code = code;
     }
 
+    public FnExpressErrorException(int code, String message, String requestString, String responseString) {
+        super(message);
+        this.code = code;
+        this.requestString = requestString;
+        this.responseString = responseString;
+    }
+
     public int getCode() {
         return code;
+    }
+
+    public String getRequestString() {
+        return requestString;
+    }
+
+    public String getResponseString() {
+        return responseString;
     }
 
     @Override
     public String toString() {
         return "FnExpressErrorException{" +
                 "code=" + code +
-                ", message=" + getMessage() +
-                "} " + super.toString();
+                ", requestString='" + requestString + '\'' +
+                ", responseString='" + responseString + '\'' +
+                '}';
     }
-
 }
